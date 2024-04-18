8aaba0fcf8b47da2bf49919b6c18641ed44f45b9
Angelo Amato <gangeloamato@gmail.com>
2024-04-17 23:58:50 -0400

    add comments an reverse_key to graph_read

--- wikispeedia/src/graph_read.rs
+++ wikispeedia/src/graph_read.rs
@@ -36,11 +36,15 @@ impl Graph {
     }
 }
 
+// creates a key from a tsv file that maps article names to numbers 0 to n-1 where n is the number of pages
 pub fn create_key(path:&str) -> HashMap<String, usize>{
     let mut key = HashMap::new();
+
+    //read file 
     let file = File::open(path).expect("Could not open file");
     let mut coded_number: usize = 0;
     let buf_reader = std::io::BufReader::new(file).lines();
+    //page name and number stored in a HashMap 
     for line in buf_reader{
         let line_str = line.expect("Error reading").trim().to_string();
         key.insert(line_str, coded_number);
@@ -48,18 +52,33 @@ pub fn create_key(path:&str) -> HashMap<String, usize>{
     }
     key 
 }
+// creates a reverse key where the number is the key and the article name is the value
+pub fn reverse_key(key: &HashMap<String, usize>) -> HashMap<usize, String>{
+    let mut reverse_key = HashMap::new();
+    for (k,v) in key.iter(){
+        reverse_key.insert(*v, k.clone());
+    }
+    reverse_key
+}
 
+// creates a graph from a tsv file with the key mapping article names to numbers
 pub fn numbered_nodes_graph(path:&str, key: HashMap<String, usize>) -> Graph{
+    // creates an edge list for the outedges of the graph
     let mut edge_list: Vec<(Vertex, Vertex)> = Vec::new();
+    // reads the file and goes through line by line
     let file = File::open(path).expect("Could not open file");
     let buf_reader = std::io::BufReader::new(file).lines();
     for line in buf_reader{
         let line_str = line.expect("Error reading");
+        // creates a 2 string vector from each line in the file
         let edge: Vec<&str> = line_str.trim().split('\t').collect();
+        // gets the number of the article from the key
         let edge_start = key.get(edge[0]).expect("Error getting key");
         let edge_end = key.get(edge[1]).expect("Error getting key");
+        // adds the edge to the edge list
         edge_list.push((*edge_start, *edge_end));
     }
+    // creates a graph from the edge list and length of the key (number of pages in the graph)
     Graph::create_directed(key.len(), &edge_list)
     
 }
